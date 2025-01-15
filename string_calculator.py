20d05d5a40621e232479e7d0f70790f4ec5034d2
Vishal Shukla <vishal.shukla@virtueanalytics.com>
2025-01-15 12:47:42 +0530

    test: adding one or two numbers returns the sum

--- string_calculator.py
+++ string_calculator.py
@@ -2,4 +2,9 @@ class StringCalculator:
     def add(self, numbers:str):
         if not numbers:
             return 0
-        
+        '''
+        Split the string by commas, convert each value to an integer, 
+        and sum them up
+        '''
+        numbers_list = map(int,numbers.split(',')) 
+        return sum(numbers_list)
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -18,3 +18,14 @@ class TestStringCalculator(unittest.TestCase):
         Expected Output: 0
         """
         self.assertEqual(self.calculator.add(""), 0)
+
+    def test_add_single_number(self):
+        """
+        Test case: Adding a single number should return the number itself.
+        Input: "1"
+        Expected Output: 1
+        """
+        self.assertEqual(self.calculator.add("1"), 1)
+
+    def test_add_two_numbers(self):
+        self.assertEqual(self.calculator.add("1,2"),3)
